f0044f885544ae4c80f71c61c9b3e2aa30a5e899
PrateekGupta <prateek.gupta1@traiana.com>
2025-05-26 16:32:03 +0530

    added changes for dynamic button craetion when certain DOM changes happen over the page

--- email-writer-chrome-extension/content.js
+++ email-writer-chrome-extension/content.js
@@ -1,6 +1,64 @@
 console.log("AI EMail Assistant is running...");
 
+function createAIButton() {
+
+const button = document.createElement('div');
+
+button.className = 'T-I J-J5-Ji aoO v7 T-I-atl L3';
+button.style.marginRight = '8px';
+button.innerHTML = 'AI Reply';
+button.setAttribute('role', 'button');
+button.setAttribute('data-tooltip', 'Generate AI Reply');
+
+return button;
+    
+}
+
+
+function findComposeToolbar() {
+    const selectors = [
+        '.btC',
+        '.aDh',
+        '[role = "toolbar"]',
+        '.gU.Up'
+    ];
+    
+    for(const selector of selectors) {
+        const toolbar = document.querySelector(selector);
+    
+        if(toolbar) {
+            return toolbar;
+        }
+    
+        return null;
+        
+    }
+}
+
 function injectButton() {
+
+    const existingButton = document.querySelector('.ai-reply-button');
+    if(existingButton) {
+        existingButton.remove();
+    }
+
+    const toolbar = findComposeToolbar();
+    if(!toolbar) {
+        console.log("toolbar not found!");
+        return;
+    }
+
+    console.log("Toolbar found adding AI button");
+    const button = createAIButton();
+
+    button.classList.add('ai-reply-button');
+
+    button.addEventListener('click', async () => {
+
+    });
+
+    toolbar.insertBefore(button, toolbar.firstChild);
+
 
 }
 
